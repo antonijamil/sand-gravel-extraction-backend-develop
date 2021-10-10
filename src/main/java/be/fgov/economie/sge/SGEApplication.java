ac409d3dc410e3dbc0618f6fa52f6e092feead58
antonijamilcheva <antonija.milcheva@gmail.com>
2021-10-10 10:15:21 +0200

    https redirect implemented

--- src/main/java/be/fgov/economie/sge/SGEApplication.java
+++ src/main/java/be/fgov/economie/sge/SGEApplication.java
@@ -1,9 +1,16 @@
 package be.fgov.economie.sge;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -35,7 +42,43 @@ public class SGEApplication {
                     .anyRequest()
                     .authenticated()
                     .and()
-                    .httpBasic();
+                    .httpBasic()
+                    .and()
+                    .cors();
         }
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+    // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+}
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8082);
+        connector.setSecure(false);
+        connector.setRedirectPort(6942);
+        return connector;
     }
 }
---
+++ src/main/java/be/fgov/economie/sge/security/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package be.fgov.economie.sge.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
